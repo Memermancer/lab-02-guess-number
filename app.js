b960a3fd830e779464c0abd7d074d2b45d290fd3
Thomas Stussi <tstussi06@gmail.com>
2020-07-07 15:58:22 -0700

    game working, need to figure out disable

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const input = document.getElementById('number-input');
 const button = document.getElementById('submit-button');
 const triesRemaining = document.getElementById('tries-remaining');
 const winLose = document.getElementById('win-lose');
-const body = document.getElementById('main-body');
-const resultsSection = document.getElementById('results-section');
-const resetButton = document.getElementById('reset');
+const guessWas = document.getElementById('guess-was');
+//const hide = document.getElementById('hideMe');
 
 //initialize state
 const correctNumber = Math.ceil(Math.random() * 20);
@@ -17,31 +16,33 @@ let remainCount = 4;
 
 button.addEventListener('click', () => {
     //convert guess to number
-    console.log(input.value);
+    //console.log(input.value);
     const userGuess = Number(input.value);
     //compare guess with compareNumbers
     const newResults = compareNumbers(userGuess, correctNumber);
     //guess too high or low? display message, decriment score
         //too low
-        if (newResults = -1) {
-            remainCount--
+    if (newResults === -1) {
+        remainCount--;
             //display message, change view
-            }
+        triesRemaining.textContent = (`${remainCount} tries remaining.`);
+        guessWas.textContent = `Your guess was too low.`;
+    }
         //too high
-        if (newResults = 1) {
-            remainCount--
+    if (newResults === 1) {
+        remainCount--;
+        triesRemaining.textContent = (`${remainCount} tries remaining.`);
+        guessWas.textContent = `Your guess was too high.`;
             //display message, change view
-        }
-        if (remainCount = 0) {
-            //lose game message and disable game
-        }
-        if (newResults = 0) {
-            //display win message and end the game
-        }
-        }
-    
-    
-
-
-
+    }
+    if (remainCount === 0) {
+        winLose.textContent = 'You Lose!';
+        //disable game
+        document.getElementById('hideMe').style.visibility = 'hidden';
+    }
+    if (newResults === 0) {
+        winLose.textContent = 'You Win!';
+        //end the game
+        document.getElementById('hideMe').style.visibility = 'hidden';
+    }
 });
